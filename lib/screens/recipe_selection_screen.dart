663aead41ceeb3254d6c9f0ca0ba1d23846176cd
Jose Tongo <josetongo2002@gmail.com>
2022-04-27 18:01:00 -0500

    finished

--- lib/screens/recipe_selection_screen.dart
+++ lib/screens/recipe_selection_screen.dart
@@ -4,6 +4,58 @@ import 'package:homebrew_dripper/models/resource.dart';
 import 'package:homebrew_dripper/screens/recipe_detail_screen.dart';
 import 'package:homebrew_dripper/utils/coffee_data.dart';
 import 'package:homebrew_dripper/utils/resource_data.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  startTime() async {
+    var duration = Duration(seconds: 10);
+    return Timer(duration, navigateToRecipeSelectionScreen);
+  }
+
+  navigateToRecipeSelectionScreen() {
+    Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => RecipeSelectionScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF4C748B),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("HOMEBREW",
+                style: TextStyle(
+                    fontFamily: 'Norwester',
+                    fontSize: 48,
+                    letterSpacing: 0.1,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white)),
+            Text("Handmade Coffee",
+                style: TextStyle(
+                    fontFamily: 'Kollektif',
+                    fontSize: 18,
+                    letterSpacing: 0.1,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white))
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class RecipeSelectionScreen extends StatelessWidget {
   @override
