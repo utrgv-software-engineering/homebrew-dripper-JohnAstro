8adc60595a07eefabdb945623a00621ae2e10436
JohnAstro <jonathan.gonzalez12@utrgv.edu>
2022-05-06 13:21:37 -0500

    changing type and added edge case

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,17 +1,18 @@
 class Product {
   String name;
-  String url;
+  Uri url;
   double price;
 
-  Product(price, url, name) {
+  Product(price, String url, name) {
     // add any rules to reject any invalid values
     // for example, reject negative price amounts
     if (price is! double) throw new ArgumentError();
     if (price < 0) throw new ArgumentError();
     if (name is! String) throw new ArgumentError();
+    if (!url.contains('https')) throw new ArgumentError();
 
     this.name = name;
-    this.url = url;
+    this.url = Uri.parse(url);
     this.price = price;
   }
 }
