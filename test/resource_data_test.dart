b04f4ffe809f233c782b01f09c86f3fa4737560f
JohnAstro <jonathan.gonzalez12@utrgv.edu>
2022-05-06 13:30:25 -0500

    fixed test

--- test/resource_data_test.dart
+++ test/resource_data_test.dart
@@ -34,23 +34,25 @@ void main() {
     // Product 0
     expect(
         resources[0].products[0].name, "Peet's Coffee Big Bang | Medium Roast");
-    expect(resources[0].products[0].price, 17.95);
+    expect(resources[0].products[0].price, 18.95);
     expect(resources[0].products[0].url,
-        'https://www.peets.com/products/big-bang');
+        Uri.parse('https://www.peets.com/products/big-bang'));
 
     // Product 1
     expect(resources[0].products[1].name,
         "Caribou Coffee French Roast Blend | Dark Roast");
     expect(resources[0].products[1].price, 14.99);
-    expect(resources[0].products[1].url,
-        'https://www.cariboucoffee.com/product/sumatra-lingtong-region/');
+    expect(
+        resources[0].products[1].url,
+        Uri.parse(
+            'https://www.cariboucoffee.com/product/sumatra-lingtong-region/'));
 
     // Product 2
     expect(resources[0].products[2].name,
         "Irving Farm Highline Blend | Light Roast");
     expect(resources[0].products[2].price, 17.35);
     expect(resources[0].products[2].url,
-        'https://www.drinktrade.com/highline/p/1526');
+        Uri.parse('https://www.drinktrade.com/highline/p/1526'));
   });
 
   test('Grinders resource should give back the grinder products in my app', () {
